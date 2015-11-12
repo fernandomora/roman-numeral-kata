dda03e387b74390e75e637a001ded4b0a0ae28d8
ScalaBcn <scalabcn@scalabcn.org>
2015-11-12 19:37:20 +0100

    two and three and five

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,12 +1,8 @@
 object RomanNumerals {
 
   def fromNumber(n: Int): String = {
-    n match {
-      case 0 => ""
-      case 1 => "I"
-      case 2 => "II"
-      case 3 => "III"
-    }
+    if (n <= 3) "I" * n
+    else "V"
   }
 
 }
--- src/test/scala/RomanNumeralSpec.scala
+++ src/test/scala/RomanNumeralSpec.scala
@@ -12,9 +12,13 @@ class RomanNumeralSpec extends FlatSpec with Matchers {
     assert(RomanNumerals.fromNumber(1) === "I")
   }
 
-  it should("two and three") in {
+  it should "two and three" in {
     assert(RomanNumerals.fromNumber(2) === "II")
     assert(RomanNumerals.fromNumber(3) === "III")
+  }
+
+  it should "five" in {
+    assert(RomanNumerals.fromNumber(5) === "V")
   }
 
 }
