9b7d8cd13aa763b8ae1b9bfae8ae62f950a37844
ScalaBcn <scalabcn@scalabcn.org>
2015-11-12 20:23:40 +0100

    refactor 3

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,12 +1,15 @@
 object RomanNumerals {
 
   def fromNumber(n: Int): String = {
-    if (n < 4) "I" * n
+    if (n == 0) ""
     else if (n == 4) "IV"
     else if (n == 9) "IX"
+
+    else if (n < 5)  "I" + fromNumber(n - 1)
     else if (n < 10) "V" + fromNumber(n - 5)
     else if (n < 50) "X" + fromNumber(n - 10)
-    else "X"
+
+    else "unknown"
   }
 
 }
