414ebcb3c6ba515c97c3c99cdaf2f91450eaa3cc
ScalaBcn <scalabcn@scalabcn.org>
2015-11-12 20:00:55 +0100

    refactor recursivitat V

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,18 +1,9 @@
 object RomanNumerals {
 
   def fromNumber(n: Int): String = {
-    if (n == 4) "IV"
-    else {
-      val s = if (n >= 5) "V" else ""
-      s + {
-        (n % 5) match {
-          case 0 => ""
-          case 1 => "I"
-          case 2 => "II"
-          case 3 => "III"
-        }
-      }
-    }
+    if (n < 4) "I" * n
+    else if (n == 4) "IV"
+    else "V" + fromNumber(n - 5)
   }
 
 }
