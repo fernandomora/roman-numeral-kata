1dc67734989907242b147d479bdaa4fe626b90ef
ScalaBcn <scalabcn@scalabcn.org>
2015-11-12 21:11:12 +0100

    Use conversions list

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,30 +1,25 @@
 object RomanNumerals {
   val conversions = List( // less than N -> "..."+from(...)
-    1 -> "",
-    4 -> "I",
-    5 -> "IV",
-    9 -> "V",
-    10 -> "IX",
-    40 -> "L",
-    50 -> "XL",
-    90 -> "L",
-    100 -> "XC",
-    400 -> "C")
+    (1, "", 0),
+    (4, "I", 1),
+    (5, "IV", 4),
+    (9, "V", 5),
+    (10,"IX", 9),
+    (40,"X", 10),
+    (50, "XL", 40),
+    (90, "L", 50),
+    (100, "XC", 90),
+    (400, "C", 100))
 
   def fromNumber(n: Int): String = {
     if (n  < 1) ""
-    else if (n < 4) "I" + fromNumber(n - 1)
-
-    else if (n < 5) "IV"
-    else if (n < 9) "V" + fromNumber(n - 5)
-
-    else if (n < 10) "IX"
-    else if (n < 40) "X" + fromNumber(n - 10)
-    else if (n < 50) "XL" + fromNumber(n - 40)
-    else if (n < 90) "L"  + fromNumber(n - 50)
-    else if (n < 100) "XC" + fromNumber(n - 90)
-    else if (n < 400) "C" + fromNumber(n - 100)
-    else "unknown"
+    else {
+      val conversion = conversions.find { case (number, _, _) => n < number }
+      conversion match {
+        case None => ""
+        case Some((_, roman, substr)) => roman + fromNumber(n - substr)
+      }
+    }
   }
 
 }
