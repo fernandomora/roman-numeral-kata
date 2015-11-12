b49168784a7f793d53e942d5981003ff5946cbe6
Fernando Mora <fernando.mora@agilogy.com>
2015-11-13 00:48:44 +0100

    Implemented in a tail-recursive way by trampolining using TailRec

--- src/main/scala/RomanNumerals.scala
+++ src/main/scala/RomanNumerals.scala
@@ -1,25 +1,28 @@
 object RomanNumerals {
 
   val conversions = List(
-    (1000,"M"),
-    (900,"CM"),
+    (1000, "M"),
+    (900, "CM"),
     (500, "D"),
     (400, "CD"),
     (100, "C"),
     (90, "XC"),
     (50, "L"),
     (40, "XL"),
-    (10,"X"),
-    (9,"IX"),
+    (10, "X"),
+    (9, "IX"),
     (5, "V"),
     (4, "IV"),
     (1, "I")
   )
 
   def fromNumber(n: Int): String = {
-    conversions.collectFirst{ case (x, roman) if n >= x =>
-      roman + fromNumber(n-x)
-    }.getOrElse("")
+    import scala.util.control.TailCalls._
+    def fromNumberAcc(n: Int, acc: String = ""): TailRec[String] = tailcall[String] {
+      conversions.collectFirst {
+        case (value, roman) if n >= value => fromNumberAcc(n - value, acc + roman)
+      }.getOrElse(done(acc))
+    }
+    fromNumberAcc(n).result
   }
-
 }
